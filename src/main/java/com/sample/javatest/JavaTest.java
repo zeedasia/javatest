dd2311f91df28f0ae5375263da2e6f6b98a24792
KC Wong <wong.k.c@zeedasia.com.hk>
2016-09-09 16:02:09 +0800

    init

---
+++ src/main/java/com/sample/javatest/JavaTest.java
@@ -0,0 +1,10 @@
+package com.sample.javatest;
+
+/**
+ * Created by kcwong on 2/9/2016.
+ */
+public class JavaTest {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
